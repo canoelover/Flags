eadbfee95c6835f6892ce492633494ccf81ab8e4
Mark Koble <canoelover@comcast.net>
2015-06-05 22:34:14 -0700

    Commit after Flag Storyboard design

--- FirstViewController.designer.cs
+++ FirstViewController.designer.cs
@@ -14,8 +14,96 @@ namespace Flags
 	[Register ("FirstViewController")]
 	partial class FirstViewController
 	{
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UILabel AnswerLabel { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIImageView FlagImageView { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIButton Guess1Button { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIButton Guess2Button { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIButton Guess3Button { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIButton Guess4Button { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIButton Guess5Button { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIButton Guess6Button { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIButton Guess7Button { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIButton Guess8Button { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIButton Guess9Button { get; set; }
+
 		void ReleaseDesignerOutlets ()
 		{
+			if (AnswerLabel != null) {
+				AnswerLabel.Dispose ();
+				AnswerLabel = null;
+			}
+			if (FlagImageView != null) {
+				FlagImageView.Dispose ();
+				FlagImageView = null;
+			}
+			if (Guess1Button != null) {
+				Guess1Button.Dispose ();
+				Guess1Button = null;
+			}
+			if (Guess2Button != null) {
+				Guess2Button.Dispose ();
+				Guess2Button = null;
+			}
+			if (Guess3Button != null) {
+				Guess3Button.Dispose ();
+				Guess3Button = null;
+			}
+			if (Guess4Button != null) {
+				Guess4Button.Dispose ();
+				Guess4Button = null;
+			}
+			if (Guess5Button != null) {
+				Guess5Button.Dispose ();
+				Guess5Button = null;
+			}
+			if (Guess6Button != null) {
+				Guess6Button.Dispose ();
+				Guess6Button = null;
+			}
+			if (Guess7Button != null) {
+				Guess7Button.Dispose ();
+				Guess7Button = null;
+			}
+			if (Guess8Button != null) {
+				Guess8Button.Dispose ();
+				Guess8Button = null;
+			}
+			if (Guess9Button != null) {
+				Guess9Button.Dispose ();
+				Guess9Button = null;
+			}
 		}
 	}
 }
